93623d30d7ccb080ad5cf13b5a60930977d4d06b
roseMunyiri <roselynecuka@gmail.com>
2026-03-17 13:34:47 +0300

    feature: Center Icons

---
+++ src/components/HeaderIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function HeaderIcon({Icon, active}) {
+  return (
+    <div className="
+        flex 
+        items-center 
+        cursor-pointer
+        md:px-10 sm:h-14 
+        md:hover:bg-gray-100
+        rounded-xl 
+        active:border-b-2 
+        active:border-purple-500 group">
+        <Icon
+        className={`h-5 text-gray-500 
+                    group-hover:text-blue-500 
+                    sm:h-10 mx-auto 
+                    ${active && 'text-blue-500'}`}
+      />
+    </div>
+  )
+}
+
+export default HeaderIcon
