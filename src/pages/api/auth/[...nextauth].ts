ac0fd67e2ce2f9027a0cbc02cc144d04e1c7eaae
roseMunyiri <roselynecuka@gmail.com>
2026-03-19 15:53:12 +0300

    feature: Configure NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    FacebookProvider({
+
+        clientId: process.env.FACEBOOK_CLIENT_ID!,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+            }),
+  ],
+};
+export default NextAuth(authOptions);
